81cd8305ff01860d4716961acab6d8aa09641e0e
ZakariaeALLALI <zakariae.allali@e-polytechnique.ma>
2025-01-20 17:11:19 +0100

    adding back files

---
+++ back-end/routes/Contact.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const Contact = require('../model/Contact');
+const router = express.Router();
+
+// Get all contacts
+router.get('/', async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    res.json(contacts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Create a new contact
+router.post('/', async (req, res) => {
+  const { name, email, message } = req.body;
+  const newContact = new Contact({ name, email, message });
+
+  try {
+    await newContact.save();
+    res.status(201).json(newContact);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
